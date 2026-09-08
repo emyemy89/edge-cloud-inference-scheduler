913c31c56a201aa266dfea5817257710bd0743d4
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-08 21:41:49 +0200

    function strings

--- nodes/node.py
+++ nodes/node.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Edge:
+class Node:
     name: str
     compute_capacity: float
     network_latency: float
@@ -15,8 +15,16 @@ class Edge:
     current_load: float = 0.0
 
     def can_handle(self, required_compute: float):
+        """
+        Assess whether this Node can handle compute requests or not.
+        """
         return self.current_load + required_compute <= self.compute_capacity
 
     def estimated_latency(self, required_compute: float):
+        """
+        Calculate the estimated latency of the Node
+        :param required_compute:
+        :return:
+        """
         compute_time = required_compute / self.compute_capacity
         return self.network_latency + compute_time
