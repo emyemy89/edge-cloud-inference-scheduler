2a7af422ab0143d22f816ab59343ca1f5b38544f
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-08 22:07:41 +0200

    add error cases for cloud and edge allocation

--- simulation/baselines.py
+++ simulation/baselines.py
@@ -10,14 +10,20 @@ def always_edge(nodes: list[Node], request: InferenceRequest) -> Node:
     """
     Always pick the first available edge
     """
-    edge_nodes = [node for node in nodes if "edge" in node.name]
+    edge_nodes = [node for node in nodes
+                  if "edge" in node.name and node.can_handle(request.required_compute)]
+    if not edge_nodes:
+        raise RuntimeError("No available edge node")
     return edge_nodes[0]
 
 def always_cloud(nodes: list[Node], request: InferenceRequest) -> Node:
     """
     Always pick cloud
     """
-    return next(node for node in nodes if node.name == "cloud")
+    cloud = next(node for node in nodes if node.name == "cloud")
+    if not cloud.can_handle(request.required_compute):
+        raise RuntimeError("Cloud can't handle request")
+    return cloud
 
 
 def lowest_latency(nodes: list[Node], request: InferenceRequest) -> Node:
