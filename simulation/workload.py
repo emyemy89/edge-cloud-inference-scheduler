466aa004026c0ba04b6bb4ad06ef25785dcd4155
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-08 21:48:23 +0200

    add function for request generation

--- simulation/workload.py
+++ simulation/workload.py
@@ -8,4 +8,16 @@ class InferenceRequest:
     request_id: int
     required_compute: float
     deadline: float
-    
+
+    def generate_requests(num_requests: int) -> list[InferenceRequest]:
+        """
+        Generate requests based on num_requests
+        """
+        return [
+            InferenceRequest(
+                request_id=i,
+                required_compute=5.0,
+                deadline=100.0,
+            )
+            for i in range(num_requests)
+        ]
