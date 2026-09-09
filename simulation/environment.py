42b369ca0c76cd97afb3d8d018844792d234e08c
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-09 17:44:50 +0200

    Create environment.py

---
+++ simulation/environment.py
@@ -0,0 +1,28 @@
+from nodes.node import Node
+from simulation.workload import InferenceRequest
+
+
+class SimulationEnvironment:
+
+    def __init__(self, nodes: list[Node]):
+        self.nodes = nodes
+
+    def reset(self):
+        """
+        Reset node loads
+        """
+        for node in self.nodes:
+            node.current_load = 0.0
+
+    def execute(self, node: Node, request: InferenceRequest,) -> float:
+        """
+        Execute a request on a node.
+        Returns estimated latency in milliseconds.
+        """
+        if not node.can_handle(request.required_compute):
+            raise RuntimeError(
+                f"{node.name} cannot handle request {request.request_id}"
+            )
+        latency = node.estimated_latency(request.required_compute)
+        node.current_load += request.required_compute
+        return latency
