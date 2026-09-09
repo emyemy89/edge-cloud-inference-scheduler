5f105eb46116c1483154375ff5c99a1d713439de
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-09 18:23:11 +0200

    update environment to add time logic

--- simulation/environment.py
+++ simulation/environment.py
@@ -1,16 +1,29 @@
+"""
+Simulation environment
+"""
+from dataclasses import dataclass
 from nodes.node import Node
 from simulation.workload import InferenceRequest
 
+@dataclass
+class ActiveRequest:
+    request: InferenceRequest
+    node: Node
+    finish_time: float
 
 class SimulationEnvironment:
 
     def __init__(self, nodes: list[Node]):
         self.nodes = nodes
+        self.current_time = 0
+        self.active_requests: list[ActiveRequest] = []
 
     def reset(self):
         """
         Reset node loads
         """
+        self.current_time = 0
+        self.active_requests.clear()
         for node in self.nodes:
             node.current_load = 0.0
 
@@ -26,4 +39,3 @@ class SimulationEnvironment:
         latency = node.estimated_latency(request.required_compute)
         node.current_load += request.required_compute
         return latency
-    
