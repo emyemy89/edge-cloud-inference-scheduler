a6fe4981b8eee8d58973695efa2914e36f36bb86
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-10 22:07:54 +0200

    create RequestResults class
    add function for average and total latency

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,1 +1,36 @@
 from dataclasses import dataclass
+
+@dataclass
+class RequestResults:
+    request_id: int
+    node_name: str
+    latency: float
+    cost: float
+    utilization: float
+    deadline: float
+    deadline_met: bool
+
+class Metrics():
+    def __init__(self):
+        self.results: list[RequestResults] = []
+
+    def record(self, request_id: int,
+        node_name: str,
+        latency: float,
+        cost: float,
+        utilization: float,
+        deadline: float
+    ):
+        self.results.append(RequestResults(request_id, node_name, latency, cost, utilization, deadline, latency<=deadline))
+
+    def average_latency(self)->float:
+        if not self.results:
+            return 0
+        return sum(result.latency for result in self.results)/len(self.results)
+
+    def total_latency(self)->float:
+        if not self.results:
+            return 0
+        return sum(result.letency for result in self.results)
+
+    
