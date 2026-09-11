9596bba7b630af63907f2cd4ba632f275eb014c6
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-11 21:41:30 +0200

    add queueing delay

--- nodes/node.py
+++ nodes/node.py
@@ -22,10 +22,14 @@ class Node:
 
     def estimated_latency(self, required_compute: float):
         """
-        Calculate the estimated latency of the Node
+        Calculate the estimated latency of the Node based on network latency and current load
         """
+        projected_load = self.current_load + required_compute
+        load_factor = projected_load / self.compute_capacity
+
         compute_time = required_compute / self.compute_capacity
-        return self.network_latency + compute_time
+        queue_delay = max(0.0, load_factor - 1.0)*100
+        return self.network_latency + compute_time + queue_delay
 
     def add_load(self, required_compute: float):
         """
