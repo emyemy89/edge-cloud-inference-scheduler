0d6ad5d8c1f89af2433a8df0d1ece35ae58f4bdb
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-09-21 16:04:46 +0200

    update parameters according to experiment

--- ml/xgboost_scheduler.py
+++ ml/xgboost_scheduler.py
@@ -15,9 +15,9 @@ def train_model(dataset):
     # Split the sets for training and testing
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
     model = XGBClassifier(
-        n_estimators=100,
+        n_estimators=300,
         max_depth=4,
-        learning_rate=0.1,
+        learning_rate=0.05,
         random_state=42,
         eval_metric="mlogloss"
         )
